fd66970fe32940a2d8e43f90861578a84366dcfd
Azron Ahyan <azronahyan@gmail.com>
2025-06-07 23:26:35 +0100

    chore: set up clean architecture project structure.

---
+++ app/src/main/java/com/hadeedahyan/sliideandroidapp/SliideAndroidApp.kt
@@ -0,0 +1,7 @@
+package com.hadeedahyan.sliideandroidapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SliideAndroidApp : Application()
---
+++ app/src/main/java/com/hadeedahyan/sliideandroidapp/presentation/screen/UserListScreen.kt
@@ -0,0 +1,9 @@
+package com.hadeedahyan.sliideandroidapp.presentation.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun UserListScreen(modifier: Modifier = Modifier) {
+
+}
